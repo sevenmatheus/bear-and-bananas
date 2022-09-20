26f0c59197cbcfc3eb6b84a33ffa695852dbca9e
Neves <sevenmatheus@outlook.com>
2022-09-20 10:50:07 -0300

    [ADD] POST to /api/mercadopago

--- api/index.js
+++ api/index.js
@@ -7,5 +7,9 @@ app.get('/api/mercadopago', (request, response) => {
     response.send('Works!')
 })
 
+app.post('/api/mercadopago', (request, response) => { 
+    response.send(request.body)
+})
+
 app.listen(port, () => console.log(`Server running in port ${port}`))
 module.exports = app;
